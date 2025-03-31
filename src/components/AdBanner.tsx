09dedbadbb0bd688ecdf26e190db18e85c749bac
Kiloukoi <benysouph@gmail.com>
2025-03-31 11:43:10 +0200

    Update AdBanner.tsx

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useRef } from 'react';
 
 interface AdBannerProps {
+  zoneId: string;
   className?: string;
 }
 
-export default function AdBanner({ className = '' }: AdBannerProps) {
+export default function AdBanner({ zoneId, className = '' }: AdBannerProps) {
   const adRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://shebudriftaiter.net/tag.min.js';
-    script.setAttribute('data-zone', '9154169');
+    script.setAttribute('data-zone', zoneId);
     document.body.appendChild(script);
 
     return () => {
       document.body.removeChild(script);
     };
-  }, []);
+  }, [zoneId]);
 
   return (
-    <div ref={adRef} className={`ad-container min-h-[100px] ${className}`}>
+    <div ref={adRef} className={`ad-container min-h-[100px] ${className}`} data-zone={zoneId}>
       {/* Monetag ads will be injected dynamically by the script */}
     </div>
   );
