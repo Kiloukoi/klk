458034225e152b04f8c40349d5bfeb46ece8931c
Kiloukoi <benysouph@gmail.com>
2025-04-21 21:17:50 +0200

    Update Kilouwers.tsx

--- src/pages/Kilouwers.tsx
+++ src/pages/Kilouwers.tsx
@@ -94,7 +94,7 @@ export default function Kilouwers() {
 
       {/* Monetization Banner */}
       <div className="mb-6">
-        <MonetizationBanner url="https://www.profitableratecpm.com/prtwx7u63?key=b8b3ba83074bbb1330d3ec04c489a161" />
+        <MonetizationBanner />
       </div>
 
       {follows.length === 0 ? (
@@ -166,6 +166,10 @@ export default function Kilouwers() {
           ))}
         </div>
       )}
+            {/* Bottom Monetization Banner */}
+      <div className="mt-8">
+        <MonetizationBanner />
+      </div>
     </div>
   );
 }
